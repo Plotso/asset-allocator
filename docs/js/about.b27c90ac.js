bcbf3ed9231838fab595cf7734f2fde2f46e38df
Plotso <ilian_1999n@abv.bg>
2025-04-05 03:06:23 +0300

    pseudo docs

---
+++ docs/js/about.b27c90ac.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkasset_allocator"]=self["webpackChunkasset_allocator"]||[]).push([[594],{584:function(a,t,n){n.r(t),n.d(t,{default:function(){return l}});var s=n(8214);const u={class:"about"};function e(a,t){return(0,s.uX)(),(0,s.CE)("div",u,t[0]||(t[0]=[(0,s.Lk)("h1",null,"This is an about page",-1)]))}var r=n(3415);const c={},o=(0,r.A)(c,[["render",e]]);var l=o}}]);
+//# sourceMappingURL=about.b27c90ac.js.map
